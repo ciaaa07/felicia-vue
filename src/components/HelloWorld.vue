80e93ed9f0a1abe1cdcafd579d237a6afe97f3bb
Felicia <felicia2912@mhs.mdp.ac.id>
2021-11-16 21:38:18 +0700

    Portofolio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,67 @@
 <template>
-  <div class="hello">
-    <h1>Felicia (2024250012)</h1>
+  <div class="container p-5 my-5 bg-dark text-white">
+  <img src="profile.jpg" class="float-start" alt="Contact" height="75" width="100">
+  <br><br>
+  <h2 class="text-center">ABOUT ME</h2>
+  <hr>
+  <pre class="text-center">
+    Haii, perkenalkan nama saya Felicia, umur 18 tahun.
+    Saya adalah seorang Mahasiswi dari Universitas MDP Palembang, jurusan Informatika.
+    Saya tinggal di Palembang bersama keluarga saya, yang terdiri dari Papa, Mama, dan adik laki - laki saya. 
+  </pre>
+</div>
+  
+  <div class="container p-5 my-5 border">
+  <img src="biodata.jpg" class="float-start" alt="Biodata" height="75" width="75">
+  <br><br>
+  <h2 class="text-center">PROFILE</h2>
+  <hr>
+  <pre>
+    Name        : Felicia
+    Birth Place : Palembang
+    Birth Date  : 29 December 2002   
+    Religion    : Buddha
+    Address     : Kolonel Atmo Gg Buntu No.419 B/5
+    Nationaly   : Indonesian
+  </pre>
   </div>
+
+<div class="container p-5 my-5 bg-dark text-white">
+  <img src="contact.jpg" class="float-start" alt="Contact" height="75" width="75">
+  <br><br>
+  <h2 class="text-center">CONTACT</h2>
+  <hr>
+  <pre>
+    <img src="wa.png" class="float-start" alt="WA" height="25" width="25"> +62 821-7538-4626
+    <br>
+    <img src="email.png" class="float-start" alt="Email" height="25" width="25"> felicia2912@mhs.mdp.ac.id
+    <br>
+    <img src="ig.png" class="float-start" alt="IG" height="25" width="25"> @felll07
+  </pre>
+</div>
+
+<div class="container p-5 my-5 border">
+  <img src="pendidikan.png" class="float-start" alt="Pendidikan" height="75" width="75">
+  <br><br>
+  <h2 class="text-center">EDUCATION</h2>
+  <hr>
+  <pre class="text-center">
+    Xaverius 2 Elementary School Palembang 
+    Xaverius Maria High School Palembang 
+    Methodist 2 Vocational High School Palembang
+    MDP University
+  </pre>
+  </div>
+
+  
+
+
+
 </template>
 
 <script>
-export default {
+export default 
+{
   name: 'HelloWorld',
   props: {
     msg: String
